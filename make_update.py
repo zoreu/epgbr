49f66bc7b4e0f1e83c49180d39a9ffe588a6ba73
Joel Silva <53149347+zoreu@users.noreply.github.com>
2023-12-29 19:49:57 -0300

    Update make_update.py

--- make_update.py
+++ make_update.py
@@ -7,14 +7,17 @@ data_atual = datetime.datetime.now(pytz.timezone('America/Sao_Paulo'))
 # Definir o formato desejado
 formato_desejado = '%Y%m%d%H%M'
 
+# Adicionar 3 horas ao horário atual
+data_com_3_horas_adicionadas = data_atual + datetime.timedelta(hours=3)
+
 # Formatar a data
-data_formatada = data_atual.strftime(formato_desejado)
+data_formatada = data_com_3_horas_adicionadas.strftime(formato_desejado)
 
 # Criar o conteúdo do arquivo de texto
 conteudo_arquivo = f'time: {data_formatada}'
 
 # Escrever o conteúdo no arquivo
 with open('update.txt', 'w') as arquivo:
-    arquivo.write(conteudo_arquivo)
+  arquivo.write(conteudo_arquivo)
 
 print('Arquivo gerado com sucesso!')
